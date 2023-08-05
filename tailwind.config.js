692603512c77b6ee48611d195105c27505314b5d
Shreyash9373 <98698979+Shreyash9373@users.noreply.github.com>
2023-08-05 12:37:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     screens:{
-      'xsm':'375px',
+      'xsm':'320px',
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
 
